cd485045c1d33c86cfc8d24e6650876e19bb8ce2
Wonmi <wonmadesign@gmail.com>
2023-02-02 14:12:22 -0600

    Create production files.

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,74 @@
+const currentTask = process.env.npm_lifecycle_event;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const fse = require('fs-extra');
 
-module.exports = {
+const config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './app/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.(s(a|c)ss)$/,
+                use: ["css-loader", "sass-loader"]
+            }
+        ]
+    }
+}
+
+class RunAfterCompile { // build에서만 적용. 'images들을 퍼블릭용 폴더로 옮기기'
+    apply(compiler) {
+      compiler.hooks.done.tap('Copy images', () => {
+        fse.copySync('./app/assets/img', './docs/assets/img');
+      });
+    }
+  }
+
+
+if(currentTask == 'dev') {
+    config.mode = 'development';
+    config.devtool = "eval-source-map";
+    config.output = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
-    }, 
-    mode: "development",
-    devtool: "eval-source-map",
-    devServer: {
+    },
+    config.devServer = {
         watchFiles: ["./app/**/*.html"],
         static: {
             directory: path.join(__dirname, "./app"),
             watch: false,
         },
         hot: true,
-        port: 3000
+        port: 3000,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        }
     },
-    module: {
-        rules: [
-            {
-                test: /\.(s(a|c)ss)$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            }
-        ]
-    }
+    config.module.rules[0].use.unshift("style-loader");
 }
+
+if(currentTask == 'build') {
+    config.mode = 'production';
+    config.devtool = "source-map";
+    config.plugins.push(
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
+        new RunAfterCompile()
+    );
+    config.optimization = {
+        splitChunks: { chunks: 'all' }
+      };
+    config.output = {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, './docs')
+      }
+    config.module.rules[0].use.unshift(MiniCssExtractPlugin.loader);
+}
+
+module.exports = config;
